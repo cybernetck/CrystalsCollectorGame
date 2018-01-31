c10d4e2b3f1fe06ffd67fe6e939b11bc55400076
cybernetck <mattswomack@gmail.com>
2018-01-31 03:22:02 -0600

    added background image, and finalized visual positioning

--- assets/javascript/crystalsGameLogic.js
+++ assets/javascript/crystalsGameLogic.js
@@ -7,6 +7,11 @@ Matthew Womack, 2018.
 
 var numberToGuess, playerScore=0, wins=0,losses=0, gem1, gem2, gem3, gem4, gem5;
 
+var userGuessRef   = $("#userGuess");
+var gameStatusRef  = $("#gameStatus");
+var numberToGetRef = $("#numberToGet");
+var userGuessRef   = $("#userGuess");
+
 function generateRandom () {
 
  numberToGuess = Math.floor(Math.random() * 120) + 19;
@@ -16,11 +21,12 @@ function generateRandom () {
  gem3 = Math.floor(Math.random() * 12) + 1;
  gem4 = Math.floor(Math.random() * 12) + 1;
 
+ numberToGetRef.html("<h3>" + numberToGuess + "</h3>")
+ debug();
 }
 
 /* first run */
 generateRandom();
-debug();
 $( "#numberToGet" ).html("<h3>" + numberToGuess + "</h3>");
 
 
@@ -36,69 +42,98 @@ function debug () {
 function startOver() {
     
     playerScore=0;
-    $( "gameStatus" ).hide();
-    $( "#userGuess" ).html(" ");
-    generateRandom();
-    debug();
+    numberToGetRef.fadeOut('slow').text(" ").fadeIn('slow', generateRandom);
+    userGuessRef.fadeOut('slow');
 
 }
+
+function winOrLose () {
+
+    if (playerScore > numberToGuess) {
+        losses += 1;
+    
+        // alert("Sorry , you lose. ");
+     
+        gameStatusRef.html("<h2> Sorry, you lose </h2>").slideDown(3000).slideUp(3000);
+     
+         startOver(); 
+    
+    }
+    
+    else if (playerScore === numberToGuess) {
+        wins += 1;
+        //alert("congrats!");
+    
+        gameStatusRef.html("<h2> Luck is with you... You've won! </h2>").slideDown(3000).slideUp(3000);
+    
+        startOver(); 
+    }
+    
+    $( "#lossesBox" ).html("<h3>" + losses + "</h3>");
+    $( "#winsBox" ).html("<h3>" + wins + "</h3>");
+    //startOver(); 
+    }
 
 /* onLick listeners */
 $( "#reset").on("click", function(){
 
+    //$("#numberToGet").text("");
+    $( "#userGuess" ).text("");
+    //gameStatusRef.text("");
+    playerScore = 0;
     generateRandom();
     debug();
-    $( "#numberToGet" ).html(" ");
-    $( "#userGuess" ).html(" ");
-    playerScore = 0;
 
-    $( "#numberToGet" ).html("<h1>" + numberToGuess + "</h1>");
+    $( "#numberToGet" ).html("<h3>" + numberToGuess + "</h3>");
+
+    $( "#gem1" ).off("click");
+    $( "#gem2" ).off("click");
+    $( "#gem3" ).off("click");
+    $( "#gem4" ).off("click");
+
     });
     
 $( "#gem1" ).on("click", function (){
+
     playerScore += gem1;
     console.log(playerScore);
-    $( "#userGuess" ).html("<h3>" + playerScore + "</h3>");
+    userGuessRef.fadeIn('fast');
+    numberToGetRef.fadeIn(2000);
+    userGuessRef.html("<h3>" + playerScore + "</h3>");
     winOrLose();
-})
+
+});
 
 $( "#gem2" ).on("click", function (){
+
     playerScore += gem2;
     console.log(playerScore);
-    $( "#userGuess" ).html("<h3>" + playerScore + "</h3>");
+    userGuessRef.fadeIn('fast');
+    numberToGetRef.fadeIn(2000);
+    userGuessRef.html("<h3>" + playerScore + "</h3>");
     winOrLose();
-})
+
+});
 
 $( "#gem3" ).on("click", function (){
+
     playerScore += gem3;
     console.log(playerScore);
-    $( "#userGuess" ).html("<h3>" + playerScore + "</h3>");
+    userGuessRef.fadeIn('fast');
+    numberToGetRef.fadeIn(2000);
+    userGuessRef.html("<h3>" + playerScore + "</h3>");
     winOrLose();
-})
+
+});
 
 $( "#gem4" ).on("click", function (){
+
     playerScore += gem4;
     console.log(playerScore);
-    $( "#userGuess" ).html("<h3>" + playerScore + "</h3>");
+    userGuessRef.fadeIn('fast');
+    numberToGetRef.fadeIn(2000);
+    userGuessRef.html("<h3>" + playerScore + "</h3>");
     winOrLose();
-})
 
-function winOrLose () {
-if (playerScore > numberToGuess) {
-    losses += 1;
+});
 
-    // alert("Sorry , you lose. ");
-    $( "#gameStatus" ).html("<h2> Sorry , you lose. </h2> ")
-    startOver(); 
-}
-else if (playerScore === numberToGuess) {
-    wins += 1;
-    //alert("congrats!");
-    $( "#gameStatus" ).html("<h2> Luck is with you... You've won! </h2>")
-    startOver(); 
-}
-
-$( "#lossesBox" ).html("<h3>" + losses + "</h3>");
-$( "#winsBox" ).html("<h3>" + wins + "</h3>");
-
-}
